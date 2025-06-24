018cbaa155ca427e8a6d07946af0cd5dd20b148d
Kamil897 <kamil2007shakirov@gmail.com>
2025-06-24 20:55:29 +0500

    1

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,9 +5,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from '../common/strategies/jwt.strategy';
 import { UsersModule } from '../users/users.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.ACCESS_TOKEN_KEY || 'SECRET_KEY',
